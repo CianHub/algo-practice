86ea30ac5ef0503f1e73f7777e1aba382a33070b
Cian O'Gralaigh <ihatecian@gmail.com>
2024-07-21 19:23:59 +0100

    improve bubble sort revision code

--- revision/bubblesort.js
+++ revision/bubblesort.js
@@ -1,12 +1,17 @@
 function bubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
+    let changes = false;
+
     for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        changes = true;
       }
     }
+
+    if (!changes) break;
   }
 
   return arr;
