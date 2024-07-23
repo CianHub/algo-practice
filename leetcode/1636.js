8515f027663bcfe5fe910ba1e8852bcffabd47dc
Cian O'Gralaigh <ihatecian@gmail.com>
2024-07-23 21:37:33 +0100

    add another solution for leetcode 1636

--- leetcode/1636.js
+++ leetcode/1636.js
@@ -38,37 +38,41 @@ function freqSelectionSort(nums) {
 function freqBubbleSort(nums) {
   let hashmap = {};
 
+  // Step 1: Create a hashmap to count the frequency of each number
   for (let i = 0; i < nums.length; i++) {
     hashmap[nums[i]] = (hashmap[nums[i]] || 0) + 1;
   }
 
+  // Step 2: Implement bubble sort based on frequency and value
   for (let i = nums.length - 1; i >= 0; i--) {
     let changes = false;
 
     for (let j = 0; j < i; j++) {
       let currentVal = nums[j];
-      let currentValFreq = hashmap[nums[j]];
+      let nextVal = nums[j + 1];
+      let currentValFreq = hashmap[currentVal];
+      let nextValFreq = hashmap[nextVal];
 
-      if (currentValFreq >= hashmap[nums[j + 1]]) {
-        if (
-          hashmap[nums[j + 1]] === currentValFreq &&
-          nums[j + 1] < currentVal
-        ) {
-          break;
-        }
-
+      // Compare based on frequency (increasing order)
+      // If frequencies are the same, compare based on value (decreasing order)
+      if (
+        currentValFreq > nextValFreq ||
+        (currentValFreq === nextValFreq && currentVal < nextVal)
+      ) {
+        // Swap values if needed
         let temp = nums[j];
         nums[j] = nums[j + 1];
         nums[j + 1] = temp;
         changes = true;
       }
     }
-    if (!changes) break;
+    if (!changes) break; // If no changes, the array is sorted
   }
 
   return nums;
 }
 
-console.log(frequencyInsertionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
-console.log(freqSelectionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
-console.log(freqBubbleSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
+//console.log(frequencyInsertionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
+//console.log(freqSelectionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
+console.log(freqBubbleSort([-1, 1, -6, 4, 5, -6, 1, 4, 1])); //[1,3,3,2,2]
+//[5,-1,4,4,-6,-6,1,1,1]...
