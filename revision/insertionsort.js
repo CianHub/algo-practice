00c5ca28106cbaca8c6e0eadfa4b923bc18eb01a
Cian O'Gralaigh <ihatecian@gmail.com>
2024-07-23 09:54:05 +0100

    add revision code for insertion dort

---
+++ revision/insertionsort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(insertionSort([5, 2, 4, 3, 1]));
