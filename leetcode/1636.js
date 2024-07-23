45585a0eab6956be7175f63d73117dabed954aff
Cian O'Gralaigh <ihatecian@gmail.com>
2024-07-23 21:25:31 +0100

    add another solution for leetcode 1636

--- leetcode/1636.js
+++ leetcode/1636.js
@@ -26,4 +26,49 @@ function frequencyInsertionSort(nums) {
   return nums;
 }
 
+function freqSelectionSort(nums) {
+  let hashmap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    hashmap[nums[i]] = (hashmap[nums[i]] || 0) + 1;
+  }
+  return nums;
+}
+
+function freqBubbleSort(nums) {
+  let hashmap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    hashmap[nums[i]] = (hashmap[nums[i]] || 0) + 1;
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    let changes = false;
+
+    for (let j = 0; j < i; j++) {
+      let currentVal = nums[j];
+      let currentValFreq = hashmap[nums[j]];
+
+      if (currentValFreq >= hashmap[nums[j + 1]]) {
+        if (
+          hashmap[nums[j + 1]] === currentValFreq &&
+          nums[j + 1] < currentVal
+        ) {
+          break;
+        }
+
+        let temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+        changes = true;
+      }
+    }
+    if (!changes) break;
+  }
+
+  return nums;
+}
+
 console.log(frequencyInsertionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
+console.log(freqSelectionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
+console.log(freqBubbleSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
