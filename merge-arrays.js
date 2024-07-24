948f1c67bf5f5c565393ea3117c1aabb285b8233
Cian O'Gralaigh <ihatecian@gmail.com>
2024-07-24 18:35:14 +0100

    add solution for merging sorted arrays

---
+++ merge-arrays.js
@@ -0,0 +1,31 @@
+function mergeSortedArrays(arr1, arr2) {
+  let mergedArr = [];
+
+  let counter1 = 0;
+  let counter2 = 0;
+
+  while (counter1 < arr1.length || counter2 < arr2.length) {
+    let currentArr1Val = arr1[counter1];
+    let currentArr2Val = arr2[counter2];
+
+    if (currentArr1Val !== undefined && currentArr2Val !== undefined) {
+      if (currentArr1Val <= currentArr2Val) {
+        mergedArr.push(currentArr1Val);
+        counter1++;
+      } else if (currentArr1Val > currentArr2Val) {
+        mergedArr.push(currentArr2Val);
+        counter2++;
+      }
+    } else if (!currentArr1Val) {
+      mergedArr.push(currentArr2Val);
+      counter2++;
+    } else {
+      mergedArr.push(currentArr1Val);
+      counter1++;
+    }
+  }
+
+  return mergedArr;
+}
+
+console.log(mergeSortedArrays([-1, 1, 2, 3, 7], [2, 4, 5, 45, 78]));
--- revision/insertionsort.js
+++ revision/insertionsort.js
@@ -1,12 +1,22 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (arr[i] < arr[j]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
+    let currentVal = arr[i];
+
+    console.log("currentVal", currentVal);
+
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > currentVal) {
+      arr[j + 1] = arr[j];
+      j--;
+
+      console.log(arr);
     }
+
+    console.log("j + 1: ", j + 1);
+    console.log("currentVal", currentVal);
+
+    arr[j + 1] = currentVal;
   }
 
   return arr;
