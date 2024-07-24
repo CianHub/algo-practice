49ae2c5350c2c6cced3ae1d4e55800820550755e
Cian O'Gralaigh <ihatecian@gmail.com>
2024-07-24 19:05:58 +0100

    add merge sort solution

---
+++ merge-sort.js
@@ -0,0 +1,43 @@
+function mergeSortedArrays(arr1, arr2) {
+  let mergedArr = [];
+
+  let counter1 = 0;
+  let counter2 = 0;
+
+  while (counter1 < arr1.length && counter2 < arr2.length) {
+    currentArr1Val = arr1[counter1];
+    currentArr2Val = arr2[counter2];
+
+    if (currentArr1Val <= currentArr2Val) {
+      mergedArr.push(currentArr1Val);
+      counter1++;
+    }
+
+    if (currentArr1Val > currentArr2Val) {
+      mergedArr.push(currentArr2Val);
+      counter2++;
+    }
+  }
+
+  while (counter1 < arr1.length) {
+    mergedArr.push(arr1[counter1]);
+    counter1++;
+  }
+
+  while (counter2 < arr2.length) {
+    mergedArr.push(arr2[counter2]);
+    counter2++;
+  }
+
+  return mergedArr;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return mergeSortedArrays(left, right);
+}
+
+console.log(mergeSort([1, 4, 7, 6, 2, 3, 5]));
