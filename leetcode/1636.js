e8c3750cbfe83431bbfcc608fd44ed86b0deacda
Cian O'Gralaigh <ihatecian@gmail.com>
2024-07-26 10:00:14 +0100

    add merge sort solution for leetcode 1636

--- leetcode/1636.js
+++ leetcode/1636.js
@@ -101,7 +101,63 @@ function freqBubbleSort(nums) {
   return nums;
 }
 
-console.log(freqInsertionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
+function freqMergeSort(nums) {
+  let hashmap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    hashmap[nums[i]] = (hashmap[nums[i]] || 0) + 1;
+  }
+
+  function mergeSortedArrays(arr1, arr2) {
+    let mergedArr = [];
+
+    let i = 0,
+      j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+      let freqi = hashmap[arr1[i]];
+      let freqj = hashmap[arr2[j]];
+
+      console.log(hashmap, freqi);
+
+      if (freqi < freqj || (freqi === freqj && arr1[i] >= arr2[j])) {
+        mergedArr.push(arr1[i]);
+        i++;
+      } else if (freqi > freqj || (freqi === freqj && arr1[i] < arr2[j])) {
+        mergedArr.push(arr2[j]);
+        j++;
+      }
+    }
+
+    while (i < arr1.length) {
+      mergedArr.push(arr1[i]);
+      i++;
+    }
+
+    while (j < arr2.length) {
+      mergedArr.push(arr2[j]);
+      j++;
+    }
+
+    return mergedArr;
+  }
+
+  function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return mergeSortedArrays(left, right);
+  }
+
+  return mergeSort(nums);
+}
+
+/*console.log(freqInsertionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
 console.log(freqSelectionSort([2, 3, 1, 3, 2])); //[1,3,3,2,2]
 console.log(freqBubbleSort([-1, 1, -6, 4, 5, -6, 1, 4, 1])); //[1,3,3,2,2]
+//[5,-1,4,4,-6,-6,1,1,1]...*/
+console.log(freqMergeSort([-1, 1, -6, 4, 5, -6, 1, 4, 1])); //[1,3,3,2,2]
 //[5,-1,4,4,-6,-6,1,1,1]...
